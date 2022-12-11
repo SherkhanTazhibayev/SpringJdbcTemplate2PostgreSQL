7785063f877146b28cbe2671d8ec2541ab337283
Sherkhan <tazhibayevsherkhan7@gmail.com>
2022-12-11 17:43:03 +0600

    Initial commit

---
+++ src/main/java/net/codejava/springjdbctemplate2postgresql/SpringJdbcTemplate2PostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package net.codejava.springjdbctemplate2postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcTemplate2PostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcTemplate2PostgreSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/codejava/springjdbctemplate2postgresql/SpringJdbcTemplate2PostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava.springjdbctemplate2postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcTemplate2PostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
